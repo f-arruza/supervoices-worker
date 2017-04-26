078d1445c0caaf854fcb0eede663cc1cc82b9685
Fernando Arruza <f.arruza@uniandes.edu.co>
2017-04-25 23:15:44 -0500

    merge08

--- Supervoices/settings/production.py
+++ Supervoices/settings/production.py
@@ -43,12 +43,41 @@ DEFAULT_FILE_STORAGE = 'Supervoices.custom_storages.MediaStorage'
 MEDIA_TMP = BASE_DIR + '/tmp/'
 
 # Memcached Config
-"""CACHES = {
+os.environ['MEMCACHE_SERVERS'] = os.environ.get('MEMCACHIER_SERVERS', '').replace(',', ';')
+os.environ['MEMCACHE_USERNAME'] = os.environ.get('MEMCACHIER_USERNAME', '')
+os.environ['MEMCACHE_PASSWORD'] = os.environ.get('MEMCACHIER_PASSWORD', '')
+
+CACHES = {
     'default': {
-        'BACKEND': 'django_elasticache.memcached.ElastiCache',
-        'LOCATION': 'svs-cache.qzzrzn.cfg.use1.cache.amazonaws.com:11211',
+        # Use pylibmc
+        'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
+
+        # Use binary memcache protocol (needed for authentication)
+        'BINARY': True,
+
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+
         'OPTIONS': {
-            'IGNORE_CLUSTER_ERRORS': [True, False],
-        },
+            # Enable faster IO
+            'tcp_nodelay': True,
+
+            # Keep connection alive
+            'tcp_keepalive': True,
+
+            # Timeout settings
+            'connect_timeout': 2000, # ms
+            'send_timeout': 750 * 1000, # us
+            'receive_timeout': 750 * 1000, # us
+            '_poll_timeout': 2000, # ms
+
+            # Better failover
+            'ketama': True,
+            'remove_failed': 1,
+            'retry_timeout': 2,
+            'dead_timeout': 30,
+        }
     }
-}"""
+}
